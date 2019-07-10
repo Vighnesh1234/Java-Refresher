7705591aaedfb98f82a496f25337abbaaf9197eb
VIGHNESH SINGH THAKUR <40726015+Vighnesh1234@users.noreply.github.com>
2019-07-09 21:35:01 -0600

    Create ReversePyramid.java
    ****
    ***
    **
    *

---
+++ ReversePyramid.java
@@ -0,0 +1,13 @@
+public class ReversePyramid{
+public static void main(String[] args)
+{
+  for(int i=0;i<=3;i++){
+  for(int j=3;j>=i;j--)
+  {
+  System.out.print("*");
+  }
+  System.out.println();
+}
+
+}
+}
